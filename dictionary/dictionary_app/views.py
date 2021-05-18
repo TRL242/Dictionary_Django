fde869960fe4187269694391a50b5c6b1fb2923c
trl242 <royaurallockhart@gmail.com>
2021-05-18 05:10:06 -0700

    added word views and made edit to index.html

--- dictionary/dictionary_app/views.py
+++ dictionary/dictionary_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from PyDictionary import PyDictionary
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def word(request):
-    return render(request, 'word.html')
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonyms(search)
+    antonyms = dictionary.antonyms(search)
+    context = {
+        'meaning': meaning,
+        'synonyms': synonyms,
+        'antonyms': antonyms,
+    }
+    return render(request, 'word.html', context)
